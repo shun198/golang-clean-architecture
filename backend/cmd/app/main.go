88c107b9333b776fa981ead1933872333447544f
shun198 <shunhiroseluvmri@gmail.com>
2025-04-29 18:59:16 +0900

    add database logic

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -4,9 +4,11 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shun198/golang-clean-architecture/internal/infrastructures/database"
 )
 
 func main() {
+	database.InitDB()
 	r := gin.Default()
 
 	if err := r.Run(":8080"); err != nil {
